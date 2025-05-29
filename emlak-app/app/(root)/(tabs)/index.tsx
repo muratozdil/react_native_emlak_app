029971c05b1a5ddf4a840b52a4eaa6c25e0504a7
Murat Özdil <akurti@Murat-MacBook-Air.local>
2025-05-29 23:56:54 +0300

    search component

--- emlak-app/app/(root)/(tabs)/index.tsx
+++ emlak-app/app/(root)/(tabs)/index.tsx
@@ -1,18 +1,41 @@
+import Search from '@/components/search';
+import icons from '@/constants/icons';
+import { useGlobalContext } from '@/lib/global-provider';
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+	Image,
+	SafeAreaView,
+	Text,
+	TouchableOpacity,
+	View,
+} from 'react-native';
 
 export default function Index() {
+	const { user, refetch } = useGlobalContext();
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: 'center',
-				alignItems: 'center',
-			}}
-		>
-			<Text className="font-bold text-lg my-10 font-rubik text-3xl">
-				Welcome to EmlakApp
-			</Text>
-		</View>
+		<SafeAreaView className="h-full bg-white">
+			<View className="px-5">
+				<View className="flex flex-row items-center justify-between mt-5">
+					<View className="flex flex-row items-center">
+						<Image
+							source={{ uri: user?.avatar }}
+							className="size-12 rounded-full"
+						/>
+						<View className="flex flex-col items-start ml-2 justify-center">
+							<Text className="font-rubik size-xs text-black-100">
+								Hello,
+							</Text>
+							<Text className="text-base text-black-300 font-rubik-medium">
+								{user?.name}
+							</Text>
+						</View>
+					</View>
+					<TouchableOpacity>
+						<Image source={icons.bell} className="size-6" />
+					</TouchableOpacity>
+				</View>
+			</View>
+			<Search />
+		</SafeAreaView>
 	);
 }
